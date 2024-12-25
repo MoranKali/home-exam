edf4b85170dcf5394049d191354fa8628636ba5d
Moran Rotman <moranrotman@gmail.com>
2024-12-25 23:08:43 +0200

    added health

--- ms1/listener.py
+++ ms1/listener.py
@@ -3,13 +3,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/health', methods=['GET'])
 @app.route('/payload', methods=['POST'])
 def get_payload():
     data = request.get_json()
     if data:
+        # add tests and send to sqs
         return jsonify({"status": "success", "data": data}), 200
     else:
         return jsonify({"status": "error", "message": "No payload received"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    payload=app.run(host='0.0.0.0', port=8080)
