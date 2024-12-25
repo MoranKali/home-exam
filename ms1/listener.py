1809ca77f8468f07f5526bd40e62ccd084572730
Moran Rotman <moranrotman@gmail.com>
2024-12-26 00:27:54 +0200

    added validation and tests

--- ms1/listener.py
+++ ms1/listener.py
@@ -1,14 +1,27 @@
 from flask import Flask, request, jsonify
+from json import dumps, loads
+from marshmallow import Schema, fields, ValidationError
 #from flask_restful import Resource, Api
 
+class PayloadSchema(Schema):
+    email_subject = fields.String(required=True)
+    email_sender = fields.String(required=True)
+    email_timestream = fields.String(required=True)
+    email_content = fields.String(required=True)
+    
 app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 @app.route('/payload', methods=['POST'])
 def get_payload():
     data = request.get_json()
+    schema=PayloadSchema()
     if data:
         # add tests and send to sqs
+        try:
+            schema.load(data)
+        except ValidationError as err:
+            return jsonify({"status": "error", "message": err.messages}), 400
         return jsonify({"status": "success", "data": data}), 200
     else:
         return jsonify({"status": "error", "message": "No payload received"}), 400
---
+++ tests/sender.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def send_payload(url, payload):
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, json=payload, headers=headers)
+    return response.status_code, response.json()
+
+def load_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+# Example usage
+if __name__ == "__main__":
+    url = "http://13.60.13.42:8080/payload"
+    file_path = "C:\\Users\\Moran\\repos\\home-exam\\tests\\example_missing_key.json"
+    payload = load_json_file(file_path)
+    status_code, response = send_payload(url, payload)
+    print(f"Status Code: {status_code}")
+    print(f"Response: {response}")
